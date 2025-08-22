68e4840da2b32d0b70af42778c047b41208c09e0
ltortladzemoesd <ltortladze.moesd@gmail.com>
2025-08-23 01:55:32 +0400

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,882 @@
+window.QUESTIONS = [
+  {
+    "id": 1,
+    "text": "What _____ y ou got in y our bag t oday?",
+    "options": [
+      "has",
+      "have",
+      "does",
+      "do"
+    ],
+    "correct": "have"
+  },
+  {
+    "id": 2,
+    "text": "W e eat fr esh fruit with some _____ for desser t.",
+    "options": [
+      "onion",
+      "salt",
+      "butter",
+      "cream"
+    ],
+    "correct": "cream"
+  },
+  {
+    "id": 3,
+    "text": "Paul and Lisa _____ lunch at the moment.",
+    "options": [
+      "eat",
+      "eats",
+      "are eating",
+      "is eating"
+    ],
+    "correct": "are eating"
+  },
+  {
+    "id": 4,
+    "text": "A ______ gr ows fruit and v egetables and looks after animals.",
+    "options": [
+      "customer",
+      "boss",
+      "farmer",
+      "mechanic"
+    ],
+    "correct": "farmer"
+  },
+  {
+    "id": 5,
+    "text": "It _____ my bir thda y yester day.",
+    "options": [
+      "is",
+      "was",
+      "are",
+      "were"
+    ],
+    "correct": "was"
+  },
+  {
+    "id": 6,
+    "text": "Iv an usually wears a r ed ______ on his head.",
+    "options": [
+      "sock",
+      "jacket",
+      "jumper",
+      "cap"
+    ],
+    "correct": "cap"
+  },
+  {
+    "id": 7,
+    "text": "Sar ah ______ t o school last week.",
+    "options": [
+      "didn't go",
+      "didn't went",
+      "doesn't go",
+      "doesn't went"
+    ],
+    "correct": "didn't go"
+  },
+  {
+    "id": 8,
+    "text": "You can send a letter at the ______ .",
+    "options": [
+      "supermarket",
+      "post office",
+      "museum",
+      "library"
+    ],
+    "correct": "post office"
+  },
+  {
+    "id": 9,
+    "text": "This is the _______ mobile phone in the shop.",
+    "options": [
+      "small",
+      "smaller",
+      "smallest",
+      "most small"
+    ],
+    "correct": "smallest"
+  },
+  {
+    "id": 10,
+    "text": "Y our _____ is y our mother ’s sister .",
+    "options": [
+      "cousin",
+      "aunt",
+      "uncle",
+      "grandmother"
+    ],
+    "correct": "aunt"
+  },
+  {
+    "id": 11,
+    "text": "Sasha ____ TV when her phone r ang.",
+    "options": [
+      "watched",
+      "was watching",
+      "watches",
+      "is watching"
+    ],
+    "correct": "was watching"
+  },
+  {
+    "id": 12,
+    "text": "My friend Tony is v ery _____. He alwa ys mak es me laugh",
+    "options": [
+      "funny",
+      "friendly",
+      "kind",
+      "popular"
+    ],
+    "correct": "funny"
+  },
+  {
+    "id": 13,
+    "text": "E very home ____ a r obot in the futur e.",
+    "options": [
+      "is having",
+      "has",
+      "will have",
+      "had"
+    ],
+    "correct": "will have"
+  },
+  {
+    "id": 14,
+    "text": "W e studied bir ds and ﬁsh in our ____ lesson t oday.",
+    "options": [
+      "chemistry",
+      "physics",
+      "biology",
+      "maths"
+    ],
+    "correct": "biology"
+  },
+  {
+    "id": 15,
+    "text": "Y ou ____ be late with y our homework.",
+    "options": [
+      "must",
+      "mustn't",
+      "don't have to",
+      "have to"
+    ],
+    "correct": "mustn't"
+  },
+  {
+    "id": 16,
+    "text": "It ’s easy t o ____ this game fr om the internet t o your smar tphone.",
+    "options": [
+      "post",
+      "search",
+      "upload",
+      "download"
+    ],
+    "correct": "download"
+  },
+  {
+    "id": 17,
+    "text": "I’v e pla yed the guitar ____ thr ee years.",
+    "options": [
+      "from",
+      "since",
+      "for",
+      "during"
+    ],
+    "correct": "for"
+  },
+  {
+    "id": 18,
+    "text": "W e ____ a gr eat time at the par ty",
+    "options": [
+      "did",
+      "were",
+      "felt",
+      "had"
+    ],
+    "correct": "had"
+  },
+  {
+    "id": 19,
+    "text": "Cheese ____ fr om milk.",
+    "options": [
+      "is made",
+      "is make",
+      "has made",
+      "makes"
+    ],
+    "correct": "is made"
+  },
+  {
+    "id": 20,
+    "text": "Y our ____ is between y our head and y our body",
+    "options": [
+      "stomach",
+      "ear",
+      "heart",
+      "neck"
+    ],
+    "correct": "neck"
+  },
+  {
+    "id": 21,
+    "text": "A ustralia isn ’t ____ Alaska.",
+    "options": [
+      "as cold",
+      "as cold as",
+      "as cold than",
+      "colder as"
+    ],
+    "correct": "as cold as"
+  },
+  {
+    "id": 22,
+    "text": "In the UK, y ou can ’t ____ school befor e you ar e 16 y ears old",
+    "options": [
+      "get",
+      "start",
+      "leave",
+      "go to"
+    ],
+    "correct": "leave"
+  },
+  {
+    "id": 23,
+    "text": "Ther e aren’t _____ places t o go skateboar ding in my t own.",
+    "options": [
+      "some",
+      "many",
+      "much",
+      "a few"
+    ],
+    "correct": "many"
+  },
+  {
+    "id": 24,
+    "text": "Could y ou ____ me a fa vour?",
+    "options": [
+      "do",
+      "make",
+      "have",
+      "give"
+    ],
+    "correct": "do"
+  },
+  {
+    "id": 25,
+    "text": "Y ou look tir ed – I think y ou ____ go t o bed now .",
+    "options": [
+      "should",
+      "shouldn't",
+      "must",
+      "mustn't"
+    ],
+    "correct": "should"
+  },
+  {
+    "id": 26,
+    "text": "Y ou can r eturn these shoes t o the shop if y ou k eep the _____.",
+    "options": [
+      "account",
+      "change",
+      "receipt",
+      "ticket"
+    ],
+    "correct": "receipt"
+  },
+  {
+    "id": 27,
+    "text": "If I ____ t oo much coff ee, I can ’t get t o sleep at night.",
+    "options": [
+      "am drinking",
+      "will drink",
+      "drank",
+      "drink"
+    ],
+    "correct": "drink"
+  },
+  {
+    "id": 28,
+    "text": "I went running this morning and now I’ m ____ .",
+    "options": [
+      "exhausted",
+      "confused",
+      "stressed",
+      "disappointed"
+    ],
+    "correct": "exhausted"
+  },
+  {
+    "id": 29,
+    "text": "This dr ess ____ b y an Italian fashion company .",
+    "options": [
+      "designed",
+      "was design",
+      "was designed",
+      "has designed"
+    ],
+    "correct": "was designed"
+  },
+  {
+    "id": 30,
+    "text": "Please ___ some juice int o each glass.",
+    "options": [
+      "ﬁll",
+      "give",
+      "pour",
+      "throw"
+    ],
+    "correct": "pour"
+  },
+  {
+    "id": 31,
+    "text": "It ’s quite cold t oday. You _________ wear a coat.",
+    "options": [
+      "should",
+      "mustn't",
+      "have to",
+      "don't have to"
+    ],
+    "correct": "should"
+  },
+  {
+    "id": 32,
+    "text": "W e pla yed really well and we easily ______ the other team.",
+    "options": [
+      "beat",
+      "won",
+      "passed",
+      "scored"
+    ],
+    "correct": "beat"
+  },
+  {
+    "id": 33,
+    "text": "W e arriv ed her e on Monda y and so far we ______ t o the beach thr ee times.",
+    "options": [
+      "went",
+      "have been",
+      "are going",
+      "were going"
+    ],
+    "correct": "have been"
+  },
+  {
+    "id": 34,
+    "text": "The riv er le vel rose b y thr ee metr es last y ear and ther e was a big _____.",
+    "options": [
+      "snowstorm",
+      "forestfire",
+      "earthquake",
+      "flood"
+    ],
+    "correct": "flood"
+  },
+  {
+    "id": 35,
+    "text": "I can ’t meet y ou tomorr ow because I _____ the football match on TV.",
+    "options": [
+      "watch",
+      "will have watches",
+      "will watch",
+      "will be watching"
+    ],
+    "correct": "will be watching"
+  },
+  {
+    "id": 36,
+    "text": "W e ha ve to check ____ at the airpor t two hours befor e our ﬂight.",
+    "options": [
+      "out",
+      "off",
+      "in",
+      "up"
+    ],
+    "correct": "in"
+  },
+  {
+    "id": 37,
+    "text": "The y found the boy ____ wallet the y found in the park.",
+    "options": [
+      "who",
+      "which",
+      "what",
+      "whose"
+    ],
+    "correct": "whose"
+  },
+  {
+    "id": 38,
+    "text": "Please ____ to the teacher for being late.",
+    "options": [
+      "apologise",
+      "complain",
+      "remind",
+      "remember"
+    ],
+    "correct": "apologise"
+  },
+  {
+    "id": 39,
+    "text": "He doesn ’t lik e his hair long, so he will ______ next week.",
+    "options": [
+      "have cut his hair",
+      "get cut his hair",
+      "have his hair cut",
+      "cut his hair"
+    ],
+    "correct": "have his hair cut"
+  },
+  {
+    "id": 40,
+    "text": "Last night I _____ I was ﬂying in a plane!",
+    "options": [
+      "imagined",
+      "dreamed",
+      "encouraged",
+      "admired"
+    ],
+    "correct": "dreamed"
+  },
+  {
+    "id": 41,
+    "text": "The r oads ____ now after the snowstorm.",
+    "options": [
+      "are repaired",
+      "are repairing",
+      "are be repaired",
+      "are being repaired"
+    ],
+    "correct": "are being repaired"
+  },
+  {
+    "id": 42,
+    "text": "Please turn the lights ____ when y ou leave.",
+    "options": [
+      "away",
+      "on",
+      "off",
+      "up"
+    ],
+    "correct": "off"
+  },
+  {
+    "id": 43,
+    "text": "I ____ t o go swimming e very Satur day , but now I go running instead.",
+    "options": [
+      "used",
+      "would use",
+      "had used",
+      "use"
+    ],
+    "correct": "used"
+  },
+  {
+    "id": 44,
+    "text": "Be careful with that v ase or y ou’ll ____ it ____ .",
+    "options": [
+      "erupt",
+      "knock over",
+      "fall down",
+      "collapse"
+    ],
+    "correct": "knock over"
+  },
+  {
+    "id": 45,
+    "text": "His passpor t is still her e, so he ______ ha ve left the countr y.",
+    "options": [
+      "might",
+      "must",
+      "can't",
+      "may"
+    ],
+    "correct": "can't"
+  },
+  {
+    "id": 46,
+    "text": "The t our guide t ook us _____ the Old Town and t old us about the local history.",
+    "options": [
+      "around",
+      "at",
+      "on",
+      "about"
+    ],
+    "correct": "around"
+  },
+  {
+    "id": 47,
+    "text": "If y ou had left home earlier , you ______ missed the tr ain.",
+    "options": [
+      "wouldn't",
+      "would have",
+      "wouldn't have",
+      "didn't"
+    ],
+    "correct": "wouldn't have"
+  },
+  {
+    "id": 48,
+    "text": "Tonia encounter ed many pr oblems on her expedition t o the N or th Pole but she showed gr eat ____ and r efused t o giv e up.",
+    "options": [
+      "deﬁnition",
+      "direction",
+      "decision",
+      "determination"
+    ],
+    "correct": "determination"
+  },
+  {
+    "id": 49,
+    "text": "The competition, _____ of swimming, running and cy cling e vents, is the hardest I’v e ever attempted.",
+    "options": [
+      "consist",
+      "consisted",
+      "consisting",
+      "consists"
+    ],
+    "correct": "consisting"
+  },
+  {
+    "id": 50,
+    "text": "A neighbour who ______ the bur glary called the police and the suspects were arr ested.",
+    "options": [
+      "charged",
+      "witnessed",
+      "sentenced",
+      "comitted"
+    ],
+    "correct": "witnessed"
+  },
+  {
+    "id": 51,
+    "text": "I don ’t want t o go t o the par ty but I can ’t really get _____.",
+    "options": [
+      "it out of",
+      "out it of",
+      "out of it",
+      "of it out"
+    ],
+    "correct": "out of it"
+  },
+  {
+    "id": 52,
+    "text": "The judges wer e very impr essed _____ her unique designs.",
+    "options": [
+      "about",
+      "of",
+      "at",
+      "by"
+    ],
+    "correct": "by"
+  },
+  {
+    "id": 53,
+    "text": "A t ﬁrst, it was diﬃcult, but now we ____ up early .",
+    "options": [
+      "used to ge",
+      "used to getting",
+      "are used to get",
+      "are used to getting"
+    ],
+    "correct": "are used to getting"
+  },
+  {
+    "id": 54,
+    "text": "My dr eam is t o _____ a diff erence in the world.",
+    "options": [
+      "make",
+      "take",
+      "do",
+      "put"
+    ],
+    "correct": "make"
+  },
+  {
+    "id": 55,
+    "text": "In ﬁfty y ears, people _____ using oil and gas for ener gy.",
+    "options": [
+      "will be stopped",
+      "will have stopped",
+      "will have been stopping",
+      "will be stopping"
+    ],
+    "correct": "will have stopped"
+  },
+  {
+    "id": 56,
+    "text": "Their concer ts alwa ys sell out quickly . We should buy our tick ets _____ advance.",
+    "options": [
+      "on",
+      "at",
+      "by",
+      "in"
+    ],
+    "correct": "in"
+  },
+  {
+    "id": 57,
+    "text": "Julie wanted t o meet the bo y _____ sent her an email.",
+    "options": [
+      "that had",
+      "he had",
+      "which had",
+      "had"
+    ],
+    "correct": "that had"
+  },
+  {
+    "id": 58,
+    "text": "Marie and Jackie ha ven’t spok en since the y fell _____ with each other last y ear.",
+    "options": [
+      "over",
+      "out",
+      "away",
+      "down"
+    ],
+    "correct": "out"
+  },
+  {
+    "id": 59,
+    "text": "Abb y ____ such a successful swimmer now if she hadn ’t practised so much as a child.",
+    "options": [
+      "won't be",
+      "wouldn't be",
+      "wouldn't have been",
+      "hadn't been"
+    ],
+    "correct": "wouldn't be"
+  },
+  {
+    "id": 60,
+    "text": "Our English exam was so easy! It _______.",
+    "options": [
+      "was a piece of cake",
+      "broke the ice",
+      "crossed my mind",
+      "was a pain"
+    ],
+    "correct": "was a piece of cake"
+  },
+  {
+    "id": 61,
+    "text": "I r eally f eel I can --------- my fa vourite football pla yer. W e’re from the same ar ea and come fr om a similar back ground.",
+    "options": [
+      "relate to",
+      "rely on",
+      "clash with",
+      "come across"
+    ],
+    "correct": "relate to"
+  },
+  {
+    "id": 62,
+    "text": "St op complaining about being cold. Y ou ----------- worn warmer clothes.",
+    "options": [
+      "must have",
+      "ought to have",
+      "needn’t have",
+      "may have"
+    ],
+    "correct": "ought to have"
+  },
+  {
+    "id": 63,
+    "text": "The main -------- of my new smar tphone was the price. It cost almost $1,000!",
+    "options": [
+      "breakthrough",
+      "cutting edge",
+      "drawback",
+      "fallout"
+    ],
+    "correct": "drawback"
+  },
+  {
+    "id": 64,
+    "text": "Sorr y, I can ’t talk right now , my online class -----------",
+    "options": [
+      "will start",
+      "was going to start",
+      "is about to start",
+      "would start"
+    ],
+    "correct": "is about to start"
+  },
+  {
+    "id": 65,
+    "text": "A man and a woman ha ve been --------- with stealing a priceless sculptur e from the t own’s museum last month.",
+    "options": [
+      "arrested",
+      "prosecuted",
+      "sentenced",
+      "charged"
+    ],
+    "correct": "charged"
+  },
+  {
+    "id": 66,
+    "text": "Under no cir cumstances ---------- tr y to boar d a tr ain when the doors are closing.",
+    "options": [
+      "should you",
+      "you should",
+      "shouldn’t you",
+      "you shouldn’t"
+    ],
+    "correct": "should you"
+  },
+  {
+    "id": 67,
+    "text": "Opening an online st ore has r eally ----------- the company ’s sales.",
+    "options": [
+      "launched",
+      "set off",
+      "taken off",
+      "boosted"
+    ],
+    "correct": "boosted"
+  },
+  {
+    "id": 68,
+    "text": "My trip t o London was mostly fun, although ---------- on the last night!",
+    "options": [
+      "I had my bag stolen",
+      "they stole my bag",
+      "I got them to steal my bag",
+      "my bag stole"
+    ],
+    "correct": "I had my bag stolen"
+  },
+  {
+    "id": 69,
+    "text": "If y ou ----------- any diﬃculties using our new app, please let us know and we ’ll help y ou out.",
+    "options": [
+      "come through to",
+      "come round about",
+      "come down for",
+      "come up against"
+    ],
+    "correct": "come up against"
+  },
+  {
+    "id": 70,
+    "text": "------------ the trophy five times, Brazil is the most successful country in the FIFA World Cup.",
+    "options": [
+      "Have won",
+      "Having won",
+      "Winning",
+      "Won"
+    ],
+    "correct": "Having won"
+  },
+  {
+    "id": 71,
+    "text": "Mr. and Mrs. Smith have three children: John, Maria and David. John, Maria and David are ------------ .",
+    "options": [
+      "brothers",
+      "sons",
+      "siblings",
+      "spouses"
+    ],
+    "correct": "siblings"
+  },
+  {
+    "id": 72,
+    "text": "It was r eally diﬃcult t o buy tick ets for the concer t because the tick et agent said it was sold out. But in the end I ------------- get some for my friends and me.",
+    "options": [
+      "managed to",
+      "could",
+      "was supposed to",
+      "was entitled to"
+    ],
+    "correct": "managed to"
+  },
+  {
+    "id": 73,
+    "text": "My family and I go t o the same island e very summer . W e love its ----- --------- beaches.",
+    "options": [
+      "undamaged",
+      "unbroken",
+      "unsettled",
+      "unspoilt"
+    ],
+    "correct": "unspoilt"
+  },
+  {
+    "id": 74,
+    "text": "W e’re going t o the park ---------- it r ains, so bring y our umbr ella.",
+    "options": [
+      "whether or not",
+      "unless",
+      "supposing",
+      "even though"
+    ],
+    "correct": "whether or not"
+  },
+  {
+    "id": 75,
+    "text": "London United football player Terence Asucre ------------ the headlines earlier this year for getting into a fight at a nightclub.",
+    "options": [
+      "hit",
+      "broke",
+      "struck",
+      "smashed"
+    ],
+    "correct": "hit"
+  },
+  {
+    "id": 76,
+    "text": "Y ou --------------- ﬁnished all y our homework alr eady . You’ve only just star ted it!",
+    "options": [
+      "shouldn’t have",
+      "mustn’t have",
+      "can’t have",
+      "needn’t have"
+    ],
+    "correct": "can’t have"
+  },
+  {
+    "id": 77,
+    "text": "I was going t o jump int o the pool fr om the highest diving boar d, but I got -------- at the last minute and couldn ’t go thr ough with it.",
+    "options": [
+      "hot legs",
+      "warm hands",
+      "cold feet",
+      "frozen toes"
+    ],
+    "correct": "cold feet"
+  },
+  {
+    "id": 78,
+    "text": "Do y ou know wher e ---------------- her clothes? She ’s alwa ys so well- dressed.",
+    "options": [
+      "is Anita buying",
+      "buys Anita",
+      "does Anita buy",
+      "Anita buys"
+    ],
+    "correct": "Anita buys"
+  },
+  {
+    "id": 79,
+    "text": "On ---------------- of the whole school, I’ d lik e to warmly welcome our new students t o London High.",
+    "options": [
+      "part",
+      "behalf",
+      "name",
+      "place"
+    ],
+    "correct": "behalf"
+  },
+  {
+    "id": 80,
+    "text": "The inter viewer ask ed the singer whether ------------- the concer t the pr evious e vening.",
+    "options": [
+      "did she enjoy",
+      "had she enjoyed",
+      "she’d enjoyed",
+      "she’s enjoyed"
+    ],
+    "correct": "she’d enjoyed"
+  }
+];
